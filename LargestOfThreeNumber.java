674701bc55e8bcdc72b66fc5b805fc3119b18267
Abhi <abhishek7340128579@gmail.com>
2021-03-26 08:07:46 +0530

    first commit

---
+++ LargestOfThreeNumber.java
@@ -0,0 +1,20 @@
+package com.abhi;
+
+import java.util.Scanner;
+
+public class LargestOfThreeNumber {
+
+    public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);
+
+	System.out.println("Enter three value");
+	int a=sc.nextInt()
+            ,b=sc.nextInt()
+            ,c=sc.nextInt();
+
+	int LargestNumber=(a>b?(a>c?a:c):(b>c?b:c));
+	
+	System.out.println(LargestNumber);
+
+    }
+}
